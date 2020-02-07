55c9071a0c946bae221e1cb514d8083d3a0082ac
Fede Mtz <44717422+mtzfederico@users.noreply.github.com>
2020-02-06 20:38:42 -0600

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,21 @@
+#/uwr/bin/env python3
+#https://www.powercms.in/blog/how-get-json-data-remote-url-python-script
+
+import urllib, json
+import requests
+
+def main():
+    url = "https://mtzfederico.com/json/example5.json"
+    response = requests.get(url)
+    data = response.json()
+
+    clientid = data["clientid"]
+    ordersList =  data["ordersList"]
+
+    print("Client ID: " + str(clientid))
+    #print(ordersList)
+    selectedItem = input("Enter the name of one of the following orders " + ordersList + ": ")
+    print(data[selectedItem])
+
+if __name__ == '__main__':
+    main()
